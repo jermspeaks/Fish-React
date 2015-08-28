f722ddaaead83b59429b8d50171bae7082460286
Jeremy Wong <jermspeaks@gmail.com>
2015-08-28 14:22:21 -0700

    added build folder

--- webpack.conf.js
+++ webpack.conf.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader' }
         ]
     }
 };
