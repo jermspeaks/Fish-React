a61d59f73bee9ee9093a8f7fbcb4cc9043d790dd
jermspeaks <jermspeaks@gmail.com>
2015-08-29 12:40:17 -0700

    styling

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,8 @@ export default class App extends Component {
   render() {
     return (
       <Template>
-        <h3>Fishes</h3>
+        <h3>Consumer Guide</h3>
+        <p>Seafood Watch recommendations show you which seafood items are "Best Choices" or "Good Alternatives," and which ones you should "Avoid."</p>
         <Fish />
       </Template>
     );
