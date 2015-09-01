b5491f5566477a5e85aec766e9bd536d6fbce00f
Jeremy Wong <jermspeaks@gmail.com>
2015-09-01 09:39:07 -0700

    fish pages

--- src/components/fish/ViewFishPage.js
+++ src/components/fish/ViewFishPage.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import fishData from '../../../data/fish.json';
 
 export default class ViewFishPage extends Component {
+  getFishData() {
+    return fishData.filter( (fish) => {
+      return fish.fishName === decodeURIComponent(this.props.fish);
+    });
+  }
+
   render() {
+    var fish = this.getFishData();
+
     return (
       <div>
-        <h1>{decodeURIComponent(this.props.fish)}</h1>
+        <h1>{fish}</h1>
         <p>Pending</p>
       </div>
     );
